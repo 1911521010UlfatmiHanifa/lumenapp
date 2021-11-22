60aa988b667ffbfea2751b18e9d19987ccc35814
Ulfatmi Hanifa <1911521010_ulfatmi@student.unand.ac.id>
2021-11-22 11:37:32 +0700

    test

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $fillable = [
-        'name', 'username', 'password'
+        'name', 'username', 'token', 'password'
     ];
 
     /**
