8d044831c647f60e01250bfcf6955afdd77cb2bc
Ulfatmi Hanifa <1911521010_ulfatmi@student.unand.ac.id>
2021-12-04 22:52:52 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -37,5 +37,12 @@ $router->group(['middleware' => 'auth'], function() use ($router){
         return response()->json($listBarang);
     });
 
+    $router->get('api/barang/{id_barang}', function ($id_barang) use ($router) {
+        $listBarang = new stdClass();
+        $results = app('db')->select("SELECT * FROM barangs where id_barang=$id_barang");
+        $listBarang->barang = $results;
+        return response()->json($listBarang);
+    });
+
     $router->post('api/logout', 'AuthController@logout');
 });
