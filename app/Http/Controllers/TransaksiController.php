0b25a08589b62979b6a1b4f0223c865d4da946a5
nikenrhmdni <nikenrhmdni09@gmail.com>
2021-12-26 23:33:04 +0700

    ubah

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -28,7 +28,7 @@ class TransaksiController extends Controller
     public function memesan (Request $request)
     {
         $alamat = $request->input('alamat');
-        $waktu = $Carbon::now()->timestamp;
+        $waktu = Carbon::now()->timestamp;
         $biaya_kirim = $request->input('biaya_kirim');
         $status_transaksi = $request->input('status_transaksi');
         $id_user = $request->input('id_user');
