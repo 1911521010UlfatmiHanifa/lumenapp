2cb232b7455d1749673aa1786cd27131a75eaf5b
Ulfatmi Hanifa <1911521010_ulfatmi@student.unand.ac.id>
2022-01-03 16:03:37 +0700

    hoo

--- routes/web.php
+++ routes/web.php
@@ -92,6 +92,13 @@ $router->group(['middleware' => 'auth'], function() use ($router){
         return response()->json(['message' => 'Barang berhasil ditambahkan ke keranjang']);
     });
 
+    $router->get('api/barang/{id_user}', function ($id_user) use ($router) {
+        $listBarang = new stdClass();
+        $results = app('db')->select("SELECT * FROM barangs WHERE id NOT IN (SELECT id_barang FROM keranjangs WHERE id_user = $id_user)");
+        $listBarang->barang = $results;
+        return response()->json($listBarang);
+    });
+
     $router->get('api/barang/{id_kategori}/{id_user}', function ($id_kategori, $id_user) use ($router) {
         $listBarang = new stdClass();
         $results = app('db')->select("SELECT * FROM barangs WHERE id NOT IN (SELECT id_barang FROM keranjangs WHERE id_user = $id_user) AND id_kategori=$id_kategori");
