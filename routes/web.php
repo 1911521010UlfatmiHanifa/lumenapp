1ded0fb9311c70c9b7b74d1353f7eeb1e863f747
Ulfatmi Hanifa <1911521010_ulfatmi@student.unand.ac.id>
2022-01-08 11:19:31 +0700

    ho

--- routes/web.php
+++ routes/web.php
@@ -126,7 +126,7 @@ $router->group(['middleware' => 'auth'], function() use ($router){
     $router->get('api/pesananSelesai/{id_user}', function ($id_user) use ($router) {
         $listPesanan = new stdClass();
         $results = app('db')->select("SELECT transaksis.id, status_transaksi, status_jemput, waktu, 
-                                    (SUM(jumlah*harga_barang))+biaya_kirim AS SUM 
+                                    (SUM(jumlah*harga_barang))+biaya_kirim AS SUM
                                     from transaksis 
                                     JOIN detail_transaksis ON transaksis.id=detail_transaksis.id_transaksi 
                                     JOIN barangs ON barangs.id=detail_transaksis.id_barang 
@@ -152,7 +152,7 @@ $router->group(['middleware' => 'auth'], function() use ($router){
         $listDPesanan = new stdClass();
         $results = app('db')->select("SELECT to_char(waktu, 'DD-Month-YYYY') AS tanggal, TO_CHAR(waktu, 'HH:mm:ss') AS waktu, alamat,
                                     biaya_kirim, SUM(jumlah*barangs.harga_barang) AS subtotal,
-                                    (SUM(jumlah*harga_barang))+biaya_kirim AS total, status_transaksi, status_jemput
+                                    (SUM(jumlah*harga_barang))+biaya_kirim AS total, status_transaksi, status_jemput, latitude, longitude
                                     FROM transaksis JOIN detail_transaksis ON transaksis.id=detail_transaksis.id_transaksi 
                                     JOIN barangs ON barangs.id=detail_transaksis.id_barang
                                     WHERE transaksis.id=$id_transaksi GROUP BY transaksis.id");
